93d66ffa57e832afe0b7776b4771490133396ce6
pizzanikita4-ops <pizzanikita4@gmail.com>
2026-03-17 02:55:47 +0500

    Update favorites.js

--- favorites.js
+++ favorites.js
@@ -1,27 +1,3 @@
-function getFavorites(){
-
-return JSON.parse(localStorage.getItem("favorites")||"[]")
-
-}
-
-function addFavorite(anime){
-
-let fav=getFavorites()
-
-if(!fav.find(a=>a.mal_id==anime.mal_id)){
-
-fav.push(anime)
-
-localStorage.setItem("favorites",JSON.stringify(fav))
-
-}
-
-}
-
-function removeFavorite(id){
-
-let fav=getFavorites().filter(a=>a.mal_id!=id)
-
-localStorage.setItem("favorites",JSON.stringify(fav))
-
-}
+function getFavorites(){ return JSON.parse(localStorage.getItem("favorites")||"[]"); }
+function addFavorite(anime){ let fav=getFavorites(); if(!fav.find(a=>a.mal_id==anime.mal_id)){ fav.push(anime); localStorage.setItem("favorites",JSON.stringify(fav)); alert("Добавлено в избранное ❤️"); } }
+function removeFavorite(id){ let fav=getFavorites().filter(a=>a.mal_id!=id); localStorage.setItem("favorites",JSON.stringify(fav)); }
