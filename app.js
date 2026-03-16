9b1cbf2bcc9b0d4c526a776986bce2cea4d7daf7
pizzanikita4-ops <pizzanikita4@gmail.com>
2026-03-17 02:55:13 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,49 +1,18 @@
-async function showHome(){
+const API="https://api.jikan.moe/v4"
 
-let data=await getTop()
-
-drawGrid(data)
-
+async function getTop(){ let r=await fetch(API+"/top/anime"); let j=await r.json(); return j.data; }
+async function randomAnime(){ let r=await fetch(API+"/random/anime"); let j=await r.json(); return j.data; }
+async function searchAnime(params){ 
+  let url=API+"/anime?";
+  if(params.q) url+="q="+encodeURIComponent(params.q)+"&";
+  if(params.type) url+="type="+params.type+"&";
+  if(params.status) url+="status="+params.status+"&";
+  if(params.year) url+="start_date="+params.year+"-01-01&end_date="+params.year+"-12-31&";
+  if(params.genres) url+="genres="+params.genres.join(",")+"&";
+  if(params.order_by) url+="order_by="+params.order_by+"&";
+  if(params.sort) url+="sort="+params.sort+"&";
+  if(params.page) url+="page="+params.page+"&";
+  let r=await fetch(url); let j=await r.json(); return j.data;
 }
-
-async function showRandom(){
-
-let a=await randomAnime()
-
-drawGrid([a])
-
-}
-
-function showSearch(){
-
-content.innerHTML=`
-
-<input id="q" placeholder="Поиск">
-
-<button onclick="doSearch()">Найти</button>
-
-<div id="results"></div>
-
-`
-
-}
-
-async function doSearch(){
-
-let q=document.getElementById("q").value
-
-let data=await searchAnime(q)
-
-drawGrid(data)
-
-}
-
-function showFavorites(){
-
-let fav=getFavorites()
-
-drawGrid(fav)
-
-}
-
-showHome()
+async function getRecommendations(id){ let r=await fetch(API+"/anime/"+id+"/recommendations"); let j=await r.json(); return j.data; }
+async function getCharacters(id){ let r=await fetch(API+"/anime/"+id+"/characters"); let j=await r.json(); return j.data; }
