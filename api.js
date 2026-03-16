30d5149f5f3305d8a66fe6b7a2e82d5c4f3647dc
pizzanikita4-ops <pizzanikita4@gmail.com>
2026-03-17 03:24:28 +0500

    Update api.js

--- api.js
+++ api.js
@@ -1,73 +1,19 @@
 const API="https://api.jikan.moe/v4"
 
-async function getTop(){
-
-let r=await fetch(API+"/top/anime")
-let j=await r.json()
-
-return j.data
-
+async function getTop(){ let r=await fetch(API+"/top/anime"); let j=await r.json(); return j.data; }
+async function randomAnime(){ let r=await fetch(API+"/random/anime"); let j=await r.json(); return j.data; }
+async function searchAnime(params){ 
+  let url=API+"/anime?";
+  if(params.q) url+="q="+encodeURIComponent(params.q)+"&";
+  if(params.type) url+="type="+params.type+"&";
+  if(params.status) url+="status="+params.status+"&";
+  if(params.year) url+="start_date="+params.year+"-01-01&end_date="+params.year+"-12-31&";
+  if(params.genres) url+="genres="+params.genres.join(",")+"&";
+  if(params.order_by) url+="order_by="+params.order_by+"&";
+  if(params.sort) url+="sort="+params.sort+"&";
+  if(params.page) url+="page="+params.page+"&";
+  let r=await fetch(url); let j=await r.json(); return j.data;
 }
-
-async function searchAnime(q){
-
-let r=await fetch(API+"/anime?q="+q)
-let j=await r.json()
-
-return j.data
-
-}
-
-async function randomAnime(){
-
-let r=await fetch(API+"/random/anime")
-let j=await r.json()
-
-return j.data
-
-}
-
-async function getRecommendations(id){
-
-let r=await fetch(API+"/anime/"+id+"/recommendations")
-let j=await r.json()
-
-return j.data
-
-}
-
-async function getCharacters(id){
-
-let r=await fetch(API+"/anime/"+id+"/characters")
-let j=await r.json()
-
-return j.data
-
-}
-
-async function getEpisodes(id){
-
-let r=await fetch(API+"/anime/"+id+"/episodes")
-let j=await r.json()
-
-return j.data
-
-}
-
-async function getReviews(id){
-
-let r=await fetch(API+"/anime/"+id+"/reviews")
-let j=await r.json()
-
-return j.data
-
-}
-
-async function getSchedule(){
-
-let r=await fetch(API+"/schedules")
-let j=await r.json()
-
-return j.data
-
-  }
+async function getGenres(){ let r=await fetch(API+"/genres/anime"); let j=await r.json(); return j.data; }
+async function getRecommendations(id){ let r=await fetch(API+"/anime/"+id+"/recommendations"); let j=await r.json(); return j.data; }
+async function getCharacters(id){ let r=await fetch(API+"/anime/"+id+"/characters"); let j=await r.json(); return j.data; }
